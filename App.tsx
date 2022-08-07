239797b1219a62e5b0272e4ef167077c732f1911
Jianlong Nie <1317272927@qq.com>
2022-08-07 16:47:02 +0800

    updates

--- App.tsx
+++ App.tsx
@@ -1,6 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import MainWebView from "./src/MainWebView";
 import AppRouter from "./src/navigation/router";
 import { observer, Provider } from "mobx-react";
 import mainStore from "./src/mobx/mainStore";
@@ -8,9 +5,9 @@ import mainStore from "./src/mobx/mainStore";
 import { spy } from "mobx";
 import { createMobxDebugger } from "mobx-flipper";
 
-if (__DEV__) {
-  spy(createMobxDebugger(mainStore));
-}
+// if (__DEV__) {
+//   spy(createMobxDebugger(mainStore));
+// }
 export default function App() {
   return (
     <Provider {...mainStore}>
@@ -18,12 +15,3 @@ export default function App() {
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
